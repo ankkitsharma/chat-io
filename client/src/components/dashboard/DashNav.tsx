3c1faf7a9d29114bdceb7096a5298e2ec74643fc
ankkitsharma <ankitsharma2801@gmail.com>
2024-10-20 17:31:41 +0530

    comment out redis configs and adapter

--- client/src/components/dashboard/DashNav.tsx
+++ client/src/components/dashboard/DashNav.tsx
@@ -9,7 +9,7 @@ type props = {
 export default function DashNav({ name, image }: props) {
   return (
     <nav className="p-6 flex justify-between items-center bg-white shadow-sm">
-      <h1 className="text-xl md:text-2xl font-extrabold">QuickChat</h1>
+      <h1 className="text-xl md:text-2xl font-extrabold">ChatIO</h1>
       <div className="flex items-center space-x-2 md:space-x-6 text-gray-700">
         <ProfileMenu name={name} image={image} />
       </div>
--- server/src/config/redis.config.ts
+++ server/src/config/redis.config.ts
@@ -1,10 +1,10 @@
-import { Redis } from "ioredis";
-
-// const redis = new Redis({
-//   host: "localhost",
-//   port: 6379,
-// });
-
-const redis = new Redis(process.env.REDIS_HOST!);
-
-export default redis;
+// import { Redis } from "ioredis";
+//
+// // const redis = new Redis({
+// //   host: "localhost",
+// //   port: 6379,
+// // });
+//
+// const redis = new Redis(process.env.REDIS_HOST!);
+//
+// export default redis;
