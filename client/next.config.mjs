e853ba0a2e94c69170a7b4049605ab113aac625e
ankkitsharma <ankitsharma2801@gmail.com>
2025-04-30 22:14:23 +0530

    fix: update API endpoint constants to use APP_URL instead of BACKEND_URL

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: false,
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:4000/api/:path*", // Proxy to Backend
+      },
+    ];
+  },
 };
 
 export default nextConfig;
--- client/src/lib/apiEndPoints.ts
+++ client/src/lib/apiEndPoints.ts
@@ -1,7 +1,7 @@
 import Env from "./env";
 
-export const BASE_URL = Env.BACKEND_URL;
-export const API_URL = BASE_URL + "/api";
+export const APP_URL = Env.APP_URL;
+export const API_URL = APP_URL + "/api";
 export const LOGIN_URL = API_URL + "/auth/login";
 export const CHAT_GROUP_URL = API_URL + "/chat-group";
 export const CHAT_GROUP_USERS_URL = API_URL + "/chat-group-users";
